aa6b93662fcb98af4c136bb0fb5ee7a92c5b6c65
sachnun <sachnun12@gmail.com>
2026-03-08 10:49:02 +0700

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -5,19 +5,17 @@ if status is-interactive
 end
 
 # PATH
-fish_add_path /root/.config/herd-lite/bin
 fish_add_path $HOME/.bun/bin
 fish_add_path /root/.opencode/bin
 
 # Environment variables
 set -gx BUN_INSTALL $HOME/.bun
-set -gx PHP_INI_SCAN_DIR "/root/.config/herd-lite/bin:$PHP_INI_SCAN_DIR"
 
 # OpenCode experimental features
 set -gx OPENCODE_EXPERIMENTAL_DISABLE_COPY_ON_SELECT true
 set -gx OPENCODE_EXPERIMENTAL_EXA true
 set -gx OPENCODE_EXPERIMENTAL_MARKDOWN true
-set -gx PENCODE_EXPERIMENTAL_PLAN_MODE true
+set -gx OPENCODE_EXPERIMENTAL_PLAN_MODE true
 
 starship init fish | source
 zoxide init fish | source
