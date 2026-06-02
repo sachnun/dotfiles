329b510afe1b2b8b756b535e7e0edb26c85ab2a3
sachnun <sachnun12@gmail.com>
2026-06-03 04:11:58 +0700

    Update .config/opencode/opencode.json Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -15,5 +15,7 @@ set -gx BUN_INSTALL $HOME/.bun
 # OpenCode experimental features
 set -gx OPENCODE_EXPERIMENTAL_DISABLE_COPY_ON_SELECT true
 set -gx OPENCODE_EXPERIMENTAL_WORKSPACES true
+set -gx OPENCODE_ENABLE_EXA true
+set -gx OPENCODE_ENABLE_PARALLEL true
 
 starship init fish | source
