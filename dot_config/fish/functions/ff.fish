1a90df574da94b720874d4ac890454baf2f8cc9d
sachnun <sachnun12@gmail.com>
2026-08-04 16:21:13 +0700

    Add .bashrc Update .config/fish/config.fish Update .config/fish/functions/ff.fish Update .config/fastfetch/config.jsonc

--- dot_config/fish/functions/ff.fish
+++ dot_config/fish/functions/ff.fish
@@ -1,7 +1,7 @@
 # ff: fastfetch wrapper, hide logo on narrow screens
 function ff
     if test "$COLUMNS" -ge 80
-        fastfetch --logo small $argv
+        fastfetch $argv
     else
         fastfetch --logo none $argv
     end
