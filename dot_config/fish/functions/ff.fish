be8ff7fde513cc24dc960a5815d6d98202c15afe
sachnun <sachnun12@gmail.com>
2026-08-04 16:38:55 +0700

    Remove .config/fish/functions/ff.fish

--- dot_config/fish/functions/ff.fish
+++
@@ -1,8 +0,0 @@
-# ff: fastfetch wrapper, hide logo on narrow screens
-function ff
-    if test "$COLUMNS" -ge 80
-        fastfetch $argv
-    else
-        fastfetch --logo none $argv
-    end
-end
