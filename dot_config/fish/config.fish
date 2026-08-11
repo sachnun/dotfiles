eb43e12368cb8f0eea45238e082732a345c04616
sachnun <sachnun12@gmail.com>
2026-08-12 01:46:32 +0700

    Update .config/fish/config.fish Remove .local/share/opencode/auth.json

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -7,19 +7,11 @@ if status is-interactive
 end
 
 # PATH
-fish_add_path $HOME/.opencode/bin
 fish_add_path $HOME/.config/herd-lite/bin
 fish_add_path $HOME/.local/bin
 fish_add_path $HOME/go/bin
 
 # Environment variables
-
-# OpenCode experimental features
-set -gx OPENCODE_EXPERIMENTAL_DISABLE_COPY_ON_SELECT true
-set -gx OPENCODE_EXPERIMENTAL_PARALLEL true
-set -gx OPENCODE_EXPERIMENTAL_SCOUT true
-set -gx OPENCODE_EXPERIMENTAL_BACKGROUND_SUBAGENTS true
-set -gx OPENCODE_EXPERIMENTAL_WORKSPACES true
 
 starship init fish | source
 
