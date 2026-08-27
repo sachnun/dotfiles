5e304aca0decee4349f98f3697f35aefb8c438a4
sachnun <sachnun12@gmail.com>
2026-08-27 14:16:37 +0000

    Update .config/fish/config.fish Update .pi/agent/auth.json

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -7,22 +7,9 @@ if status is-interactive
 end
 
 # PATH
-fish_add_path $HOME/.config/herd-lite/bin
 fish_add_path $HOME/.local/bin
-fish_add_path $HOME/go/bin
-fish_add_path $HOME/.opencode/bin
 fish_add_path $HOME/.local/share/mise/shims
 
 # Environment variables
 
 starship init fish | source
-
-
-
-# Pi
-fish_add_path "/usr/bin"
-
-
-# >>> grok installer >>>
-fish_add_path $HOME/.grok/bin
-# <<< grok installer <<<
