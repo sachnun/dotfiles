f24af097c25e12b29538cc00780c481f505b6f30
sachnun <sachnun12@gmail.com>
2026-08-29 11:09:36 +0700

    Update .config/fish/config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -4,6 +4,17 @@ umask 0022
 
 mise activate fish | source
 
+function outdated
+    if not set -qU outdated_day; or test "$outdated_day" != (date +%F)
+        set -U outdated_day (date +%F)
+        chezmoi update
+        if not mise upgrade -n --dry-run-code >/dev/null 2>&1
+            mise upgrade -n
+        end
+        clear
+    end
+end
+
 if status is-interactive
     outdated
     fastfetch
