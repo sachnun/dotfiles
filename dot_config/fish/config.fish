5a1403be55e36eb7cc9452d0258cb6d1820d963f
sachnun <sachnun12@gmail.com>
2026-08-30 02:58:16 +0700

    Update .config/fish/config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,4 +1,6 @@
-set -U fish_greeting
+function fish_greeting
+    chezmoi update
+end
 
 fish_add_path /home/linuxbrew/.linuxbrew/bin
 fish_add_path $HOME/.local/bin
@@ -6,11 +8,6 @@ fish_add_path $HOME/.local/bin
 umask 0022
 
 mise activate fish | source
-
-if status --is-login
-    chezmoi update
-    clear
-end
 
 function on_exit --on-event fish_exit
     chezmoi re-add
