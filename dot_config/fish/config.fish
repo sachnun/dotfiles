6c737bd4ac8461a1b1f7a06e9470f94b465cd3e8
sachnun <sachnun12@gmail.com>
2026-08-30 02:55:38 +0700

    Update .config/fish/config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -7,22 +7,14 @@ umask 0022
 
 mise activate fish | source
 
-function outdated
-    if not set -qU outdated_day; or test "$outdated_day" != (date +%F)
-        set -U outdated_day (date +%F)
-        chezmoi update
-        mise bootstrap packages up --yes
-        mise up --yes
-        clear
-    end
-end
-
-if status is-interactive
-    outdated
-    fastfetch
-    starship init fish | source
+if status --is-login
+    chezmoi update
+    clear
 end
 
 function on_exit --on-event fish_exit
     chezmoi re-add
 end
+
+fastfetch
+starship init fish | source
