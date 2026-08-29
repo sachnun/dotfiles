228224e0fdbfe43311fabfd6d795bcbb1872f0d2
sachnun <sachnun12@gmail.com>
2026-08-29 11:19:53 +0700

    Update .config/fish/config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -8,9 +8,8 @@ function outdated
     if not set -qU outdated_day; or test "$outdated_day" != (date +%F)
         set -U outdated_day (date +%F)
         chezmoi update
-        if not mise upgrade -n --dry-run-code >/dev/null 2>&1
-            mise upgrade -n
-        end
+        mise bootstrap packages up --yes
+        mise up --yes
         clear
     end
 end
