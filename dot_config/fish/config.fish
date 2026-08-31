64de173e24da846e9f8b177381139bbd4a2b7d58
sachnun <sachnun12@gmail.com>
2026-09-01 05:38:17 +0700

    Update .config/fish/config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -9,21 +9,8 @@ fish_add_path $HOME/.local/bin
 
 if status is-interactive
   mise activate fish | source
-  function on_exit --on-event fish_exit
-      chezmoi re-add
-  end
 else
   mise activate fish --shims | source
 end
 
 starship init fish | source
-
-if status is-interactive
-    set -l marker /run/user/(id -u)/chezmoi-update-ran
-
-    if not test -f $marker
-        touch $marker
-        setsid /root/.local/bin/chezmoi update --no-tty >/dev/null 2>&1 &
-    end
-end
-
