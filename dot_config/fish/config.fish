33c7d79ed39a6f22bc306c5d0f8dfed15d91594f
sachnun <sachnun12@gmail.com>
2026-09-02 01:02:13 +0000

    Update .config/fish/config.fish Update .config/mise/config.toml

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -7,6 +7,8 @@ umask 0022
 fish_add_path /home/linuxbrew/.linuxbrew/bin
 fish_add_path $HOME/.local/bin
 
+source $__fish_config_dir/env.fish
+
 if status is-interactive
   mise activate fish | source
 else
