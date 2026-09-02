2d604426a2b4e05315b36d6387ee0338458a4dea
sachnun <sachnun12@gmail.com>
2026-09-03 03:54:42 +0700

    Update .config/fish/config.fish Update .config/herdr/config.toml

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,8 +1,5 @@
 function fish_greeting
-    set -l pp (ps -o ppid= -p $fish_pid | string trim)
-    if not string match -q herdr (ps -o comm= -p $pp)
-        fastfetch
-    end
+    fastfetch
 end
 
 umask 0022
@@ -13,9 +10,9 @@ fish_add_path $HOME/.local/bin
 source $__fish_config_dir/env.fish
 
 if status is-interactive
-  mise activate fish | source
+    mise activate fish | source
 else
-  mise activate fish --shims | source
+    mise activate fish --shims | source
 end
 
 starship init fish | source
